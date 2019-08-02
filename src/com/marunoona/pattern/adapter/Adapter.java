94cfcbedf2f74c656b9ebdac2dff2f02969725d6
marunoona <ye_jin92@naver.com>
2019-08-02 16:59:18 +0900

    Modify Adapter Pattern

--- src/com/marunoona/pattern/adapter/Adapter.java
+++ src/com/marunoona/pattern/adapter/Adapter.java
@@ -1,7 +1,12 @@
 package com.marunoona.pattern.adapter;
 
+/**
+ *[어댑터 패턴]
+ * 한 클래스의 인터페이스를 클라이언트에서 사용하고자 다른 인터페이스로 변환하는 패턴
+ */
 public interface Adapter {
-
+    //사용 하기를 원하는 기능
     public Float twiceOf(Float f);
+    //사용 하기를 원하는 기능
     public Float halfOf(Float f);
 }
